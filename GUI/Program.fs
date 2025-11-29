0cbbc9e8eeeac83b5c0024547bd666cb09dc6749
ali <ali_20220299@fci.helwan.edu.eg>
2025-11-30 01:33:12 +0200

    feat: add initial Program.fs for Avalonia application entry point

---
+++ GUI/Program.fs
@@ -0,0 +1,22 @@
+﻿namespace SimpleStoreSimulator.GUI
+
+open System
+open Avalonia
+
+module Program =
+
+    [<CompiledName "BuildAvaloniaApp">] 
+    let buildAvaloniaApp () = 
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace(areas = Array.empty)
+
+    [<EntryPoint; STAThread>]
+    let main argv =
+        try
+            buildAvaloniaApp().StartWithClassicDesktopLifetime(argv)
+        with ex ->
+            printfn "CRITICAL ERROR: %O" ex
+            1
